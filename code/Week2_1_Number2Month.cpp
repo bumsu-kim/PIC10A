defcdcc590c8cccb0abf2d0649c0553d4d2132f2
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-10-03 23:56:20 -0700

    Week 2 Tu exercise added

---
+++ code/Week2_1_Number2Month.cpp
@@ -0,0 +1,15 @@
+/*
+* PIC 10A 2B, Fall 2022, UCLA
+* TA: Bumsu Kim
+*
+* Exercise: Number to month names
+*
+* Write a program that transforms numbers 1, 2, ..., 12 into
+* the corresponding month names January, ..., December.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
