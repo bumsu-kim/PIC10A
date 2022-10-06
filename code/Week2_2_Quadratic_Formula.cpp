49ce5afa97652ae5a460bc60717d83a35a22905b
bumsu-kim <49388481+bumsu-kim@users.noreply.github.com>
2022-10-06 02:28:18 -0700

    quad formula exercise added

---
+++ code/Week2_2_Quadratic_Formula.cpp
@@ -0,0 +1,17 @@
+/*
+* PIC 10A 2B, Fall 2022, UCLA
+* TA: Bumsu Kim
+*
+* Exercise: Quadratic formula
+*
+* Write a program that prints all solutions to the quadratic equation
+* ax^2 + bx + c = 0. Read in a, b, c and use the quadratic formula.
+* If the discriminant b^2 - 4ac is negative, display a message 
+* stating that there are no solutions.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
